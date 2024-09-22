3b51c632b86e70f3abbb9aa4778061a1d24b804a
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-09-22 13:41:34 +0530

    Fixed login

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,75 +1,92 @@
-import {NextAuthOptions} from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from "bcryptjs";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
 import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/models/user.model";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs"
 
-export const authOptions: NextAuthOptions ={
-    providers:[
-        CredentialsProvider({
-            id:"credentials",
-            name:"Credentials",
-            credentials: {
-                email: { label: "Email", type: "email" },
-                password: { label: "Password", type: "password" }
-            },
+export const authOptions:NextAuthOptions={
+  providers:[
+    //id,name,credentials,authorize method
+     CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+      email: { label: 'Email', type: 'text' },
+      password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials:any):Promise<any>{
+       await dbConnect();
+       //in this method basically apply all the validations and return the user
 
-            async authorize(credentials:any):Promise<any>{
-                await dbConnect();
-                try {
-                    const user=await UserModel.findOne({
-                        $or:[
-                            {email:credentials.identifier},
-                            {username:credentials.identifier},
-                        ]
-                    })
-                    if(!user){
-                        throw new Error("No user found")
-                    }
+       //access email and username: .credential.identifier 
+       //password have a different method
+       try{
+       const user = await User.findOne({
+        $or:[
+          {email:credentials.identifier},
+          {username: credentials.identifier}
+        ]
+       })
 
-                    if(!user.isVerified){
-                        throw new Error("User is not verified")
-                    }
+       if(!user){
+        throw new Error("No user found with this email or username")
+       }
 
-                    const isPasswordCorrect=await bcrypt.compare(credentials.password,user.password);
+       if(!user.isVerified){
+        throw new Error("Please verify your email")
+       }
+       //check password
 
-                    if(isPasswordCorrect){
-                        return user;
-                    }else{
-                        throw new Error("Password is incorrect")
-                    }
-
-                } catch (error:any) {
-                    throw new Error("An error occurred while logging in")
-                }
-            }
-        })
-    ],
-    callbacks:{
-        async jwt({ token, user }) {
-            if (user) {
-                token._id = user._id?.toString();
-                token.isVerified=user.isVerified;
-                token.isAcceptingMessages=user.isAcceptingMessages;
-                token.username=user.username;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            if(token){
-                session.user._id=token._id;
-                session.user.isVerified=token.isVerified;
-                session.user.isAcceptingMessages=token.isAcceptingMessages;
-                session.user.username=token.username;
-            }
-            return session
-        },
+       const isPasswordCorrect = await bcrypt.compare(credentials.password,user.password)
+      
+       if(isPasswordCorrect){
+        return user
+       }else{
+        throw new Error("Incorrect password")
+       }
+      }catch(err:any){
+       throw new Error(err)
+      }
+      }
+     })
+  ],
+  callbacks:{
+    //total 4--> signin redirect jwt session
+    //mainly 2 used -->jwt and session
+    //add more of the data to token and same for sessions
+    async jwt({ token, user}) {
+      if(user){
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified
+        token.isAcceptingMessages = user.isAcceptingMessages
+        token.username = user.username
+      }
+      return token
     },
-    pages:{
-        signIn:"/sign-in",
+    async session({ session, token }) {
+     if(token){
+      session.user._id = token._id;
+      session.user.isVerified = token.isVerified
+      session.user.isAcceptingMessages = token.isAcceptingMessages
+      session.user.username = token.username
+     }
+     return session
     },
-    session:{
-        strategy:"jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET
+    //impp*** 
+    //in declaration file where the interface is declar3eed 
+    //best practice --> Interface with capital letter Session
+    // variable name small letter session
+   
+  },
+  //basicallly the pages where u want this to run
+  pages:{
+    signIn:"/sign-in"
+  },
+  //databases or jwt
+  session:{ 
+    strategy:"jwt"
+  },
+  //the scret tken stored on server side
+  secret:process.env.NEXTAUTH_SECRET
+  
 }
