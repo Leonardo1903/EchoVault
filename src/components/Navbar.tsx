eea7d4f512828919e2e2ebd335cb01cbcc1a4729
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-11-01 15:26:18 +0530

    Update Navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,10 @@ import React from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { Button } from "./ui/button";
-import { User } from "next-auth";
 import { MessageSquare } from "lucide-react";
 
 function Navbar() {
   const { data: session } = useSession();
-  const user: User = session?.user as User;
 
   return (
     <header className="px-4 lg:px-6 h-16 flex items-center border-b bg-gray-800 text-white border-gray-800">
@@ -18,40 +16,44 @@ function Navbar() {
         <span className="font-bold text-xl">Echovault</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link
-          className="text-sm font-medium hover:text-teal-400"
-          href="#features"
-        >
-          Features
-        </Link>
-        <Link
-          className="text-sm font-medium hover:text-teal-400"
-          href="#how-it-works"
-        >
-          How It Works
-        </Link>
-        <Link
-          className="text-sm font-medium hover:text-teal-400"
-          href="#testimonials"
-        >
-          Testimonials
-        </Link>
-        <Link className="text-sm font-medium hover:text-teal-400" href="#faq">
-          FAQ
-        </Link>
+        {!session && (
+          <>
+            <Link
+              className="text-sm font-medium hover:text-teal-400"
+              href="#features"
+            >
+              Features
+            </Link>
+            <Link
+              className="text-sm font-medium hover:text-teal-400"
+              href="#how-it-works"
+            >
+              How It Works
+            </Link>
+            <Link
+              className="text-sm font-medium hover:text-teal-400"
+              href="#testimonials"
+            >
+              Testimonials
+            </Link>
+            <Link
+              className="text-sm font-medium hover:text-teal-400"
+              href="#faq"
+            >
+              FAQ
+            </Link>
+          </>
+        )}
       </nav>
       <div className="ml-4 flex items-center gap-2">
         {session ? (
-          <>
-            <span className="mr-4">Welcome, {user.username || user.email}</span>
-            <Button
-              onClick={() => signOut()}
-              className="bg-teal-600 text-white hover:bg-teal-700"
-              variant="outline"
-            >
-              Logout
-            </Button>
-          </>
+          <Button
+            onClick={() => signOut()}
+            className="bg-teal-600 text-white hover:bg-teal-700"
+            variant="outline"
+          >
+            Logout
+          </Button>
         ) : (
           <Link href="/sign-in">
             <Button
